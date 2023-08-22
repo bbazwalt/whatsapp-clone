44ba3fb63564b31bed96b70d85806a95b51c0d85
Benilton Azwalt <111267278+bbazwalt@users.noreply.github.com>
2023-08-22 23:08:05 +0530

    added and changed token, response, user packages

--- src/main/java/com/whatsappclone/service/UserService.java
+++ src/main/java/com/whatsappclone/service/UserService.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 import com.whatsappclone.exception.UserException;
 import com.whatsappclone.modal.User;
+import com.whatsappclone.request.UpdateUserRequest;
 
 public interface UserService {
 	
-	public User findUserById(Integer id);
+	public User findUserById(Integer id) throws UserException;
 	
-	public User findUserProfile(String jwt);
+	public User findUserProfile(String jwt) throws UserException;
 	
-	public User updateUser(Integer userId, UpdateUserRequest req) throws UserException;
+	public User updateUser(Integer userId, UpdateUserRequest req) throws UserException ;
 	
 	public List<User> searchUser(String query);
 	
--- src/main/java/com/whatsappclone/service/UserServiceImplementation.java
+++ src/main/java/com/whatsappclone/service/UserServiceImplementation.java
@@ -1,45 +1,74 @@
 package com.whatsappclone.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.security.authentication.BadCredentialsException;
+
+import com.whatsappclone.config.TokenProvider;
 import com.whatsappclone.exception.UserException;
 import com.whatsappclone.modal.User;
 import com.whatsappclone.repository.UserRepository;
 import com.whatsappclone.request.UpdateUserRequest;
 
-public class UserServiceImplementation implements UserService{
+public class UserServiceImplementation implements UserService {
 
 	private UserRepository userRepository;
-	
-	public UserServiceImplementation(UserRepository userRepository) {
+	private TokenProvider tokenProvider;
+
+	public UserServiceImplementation(UserRepository userRepository, TokenProvider tokenProvider) {
 		this.userRepository = userRepository;
-	}
-	
-	@Override
-	public User findUserById(Integer id) {
-		// TODO Auto-generated method stub
-		Optional<User> opt = userRepository.f
-		return null;
+		this.tokenProvider = tokenProvider;
 	}
 
 	@Override
-	public User findUserProfile(String jwt) {
-		// TODO Auto-generated method stub
-		return null;
+	public User findUserById(Integer id) throws UserException {
+		Optional<User> opt = userRepository.findById(id);
+
+		if (opt.isPresent()) {
+			return opt.get();
+		}
+
+		throw new UserException("No user found with the id" + id);
+
+	}
+
+	@Override
+	public User findUserProfile(String jwt) throws UserException {
+		String email = tokenProvider.getEmailFromToken(jwt);
+		
+		if(email==null) {
+			throw new BadCredentialsException("Invalid Token");
+		}
+		User user = userRepository.findByEmail(email);
+		
+		if(user==null) {
+			throw new UserException("No user found with the email" + email);
+		}
+		
+		return user;
 	}
 
 	@Override
 	public User updateUser(Integer userId, UpdateUserRequest req) throws UserException {
-		// TODO Auto-generated method stub
-		return null;
+		User user = findUserById(userId);
+		
+		if(req.getFull_name()!=null) {
+			user.setFull_name(req.getFull_name());
+		}
+		
+		if(req.getProfile_picture()!=null) {
+			user.setProfile_picture(req.getProfile_picture());
+		}
+		
+		return userRepository.save(user);
 	}
 
 	@Override
 	public List<User> searchUser(String query) {
-		// TODO Auto-generated method stub
-		return null;
+		List <User> users = userRepository.searchUser(query);
+		return users;
+		
 	}
-	
-
 
 }
