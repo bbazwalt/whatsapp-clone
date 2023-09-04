1de81776d891aad8d6c3a714ace77ff545f7a9d7
Benilton Azwalt <111267278+bbazwalt@users.noreply.github.com>
2023-09-04 22:26:07 +0530

    modifed redux action in chat

--- whatsapp-frontend/src/redux/chat/action.js
+++ whatsapp-frontend/src/redux/chat/action.js
@@ -4,8 +4,10 @@ const createChat = (chatData)=> async(dispatch)=>{
     try{
         const res = await fetch(`${BASE_API_URL}/api/v1/chats/single`,{
             headers:{
+                "Content-Type":"application/json",
                 Authorization : `Bearer ${chatData.token}`,
-            }
+            },
+            body:JSON.stringify(chatData.data)
         })
 
     }catch(error){
