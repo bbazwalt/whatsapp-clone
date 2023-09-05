cfb4d303057eed3d9dd79805681c22daeec7c8d0
Benilton Azwalt <111267278+bbazwalt@users.noreply.github.com>
2023-09-05 22:18:53 +0530

    modified chat redux files

--- whatsapp-frontend/src/redux/chat/action.js
+++ whatsapp-frontend/src/redux/chat/action.js
@@ -1,17 +1,53 @@
-import { BASE_API_URL } from "../../config/api"
+import { BASE_API_URL } from "../../config/api";
+import { CREATE_CHAT, CREATE_GROUP, GET_USERS_CHAT } from "./actiontype";
 
-const createChat = (chatData)=> async(dispatch)=>{
-    try{
-        const res = await fetch(`${BASE_API_URL}/api/v1/chats/single`,{
-            headers:{
-                "Content-Type":"application/json",
-                Authorization : `Bearer ${chatData.token}`,
-            },
-            body:JSON.stringify(chatData.data)
-        })
+export const createChat = (chatData) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/v1/chats/single`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${chatData.token}`,
+      },
+      body: JSON.stringify(chatData.data),
+    });
+    const data = await res.json();
+    console.log("create chat", data);
+    dispatch({ type: CREATE_CHAT, payload: data });
+  } catch (error) {
+    console.log("catch error", error);
+  }
+};
 
-    }catch(error){
+export const createGroupChat = (chatData) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/v1/chats/group`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${chatData.token}`,
+      },
+      body: JSON.stringify(chatData.data),
+    });
+    const data = await res.json();
+    console.log("create group", data);
+    dispatch({ type: CREATE_GROUP, payload: data });
+  } catch (error) {
+    console.log("catch error", error);
+  }
+};
 
-    }
-
-}
+export const getUsersChat = (chatData) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/v1/chats/user`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${chatData.token}`,
+      },
+      body: JSON.stringify(chatData.data),
+    });
+    const data = await res.json();
+    console.log("get users chat", data);
+    dispatch({ type: GET_USERS_CHAT, payload: data });
+  } catch (error) {
+    console.log("catch error", error);
+  }
+};
